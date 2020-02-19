594de330e92900bc290fa0f88b2fe9277068006d
manonm54 <manon.9709@gmail.com>
2020-02-19 10:21:08 +0100

    simplification

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+const baseUrl = "http://api.openweathermap.org/data/2.5";
+const appId = "&appid=d395b164d35cd41872a8e4338c93c7ef";
+
 export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=Quebec&appid=d395b164d35cd41872a8e4338c93c7ef");
+    return axios.get(baseUrl+"/weather?q=Quebec"+appId);
 }
