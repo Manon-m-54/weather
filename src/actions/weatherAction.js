a9ca824779b470a49ffd3d81ae623c70e7e4eaf9
manonm54 <manon.9709@gmail.com>
2020-02-19 15:48:40 +0100

    add something

--- src/actions/weatherAction.js
+++ src/actions/weatherAction.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const baseUrl = "http://api.openweathermap.org/data/2.5";
 const appId = "&appid=d395b164d35cd41872a8e4338c93c7ef";
 
-export function getWeather(){
-    return axios.get(baseUrl+"/weather?q=Quebec"+appId);
+export function getWeather(city){
+    return axios.get(baseUrl+"/weather?q="+city+appId);
 }
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -5,13 +5,14 @@ import {getWeather} from "../actions/weatherAction"
 function Weather() {
 
     const[weather, setWeather] = useState(null);
+    const [city, setCity] = useState("London");
 
     useEffect(() =>{
         loadWeatherData();
-    }, [])
+    }, [city])
     
     async function loadWeatherData(){
-        const weatherAjax = await getWeather();
+        const weatherAjax = await getWeather(city);
         setWeather(weatherAjax.data);
     }
 
@@ -19,8 +20,16 @@ function Weather() {
         return Math.round(tempKelvin - 273.15);
     }
 
+    function mpsToKmh(mthToKmh){
+        return Math.round(mthToKmh * 3.6);
+    }
+
     function loadInconWeather(idIcon){
         return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png";
+    }
+
+    function changeCity(city){
+        setCity(city);
     }
     return (
         <div>
@@ -30,9 +39,15 @@ function Weather() {
                     <h1>Météo : {weather.name}</h1>
                     <img src={loadInconWeather(weather.weather[0].icon)} alt="Not Found"/>
                     <p>{weather.weather[0].description}</p>
+                    <input type="text" name="ville" placeholder="Enter your town" onChange = {(event) => {changeCity(event.target.value)}}/>
                     <p>{kelvinToCelsius(weather.main.temp)} °C</p>
+                    <p>Humidité : {weather.main.humidity} %</p>
+                    <p>Vitesse du vent : {mpsToKmh(weather.wind.speed)} km/h</p>
+                    <p>Température ressentie : {kelvinToCelsius(weather.main.feels_like)} °C</p>
+                    <p>{weather.coord.lon}</p>
+                    <p>{weather.coord.lat}</p>
                 </div>
-                
+
                 : <h1>Météo en attente de chargement</h1>
             }
             
